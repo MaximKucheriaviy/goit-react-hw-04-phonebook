a3017fa9206a5bec7e07294ef160b163d08f3092
Maxim Kucheriaviy <maxim-k@i.ua>
2022-09-29 13:18:10 +0300

    refactored Add number form

--- src/components/AddNumberForm/AddNumberForm.jsx
+++ src/components/AddNumberForm/AddNumberForm.jsx
@@ -1,62 +1,69 @@
 import { nanoid } from "nanoid";
-import { Component } from "react";
+import { useState, useRef } from "react";
 import { AddForm } from "./AddNumberForm.styled"
 import PropTypes from "prop-types";
 
+export const AddNumberForm = ({onSubmit}) => {
+    const nameInputId = useRef(nanoid());
+    const telephoneInputId = useRef(nanoid());
 
-export class AddNumberForm extends Component{
-    nameInputId = nanoid();
-    telephoneInputId = nanoid();
-    state = {
-        name: '',
-        number: ''
-    }
-    chageHendler = (event) => {
+    const [name, setName] = useState("");
+    const [number, setNumber] = useState("");
+
+    const chageHendler = (event) => {
         const {value, name} = event.target;
-        this.setState({[name]: value})
+        switch(name){
+            case "name":
+                setName(value);
+                break;
+            case "number":
+                setNumber(value);
+                break;
+            default:
+                return;
+        }
     }
-    clearForm = () => {
-        this.setState({
-            name: '',
-            number: ''
-        })
+
+    const clearForm = () => {
+        setName("");
+        setNumber("");
     }
-    onSubmitHendler = (event) =>{
+
+    const onSubmitHendler = (event) =>{
         event.preventDefault();
-        this.props.onSubmit(this.state);
-        this.clearForm();
+        onSubmit({name, number});
+        clearForm();
     }
-    render(){
-        return(
-            <AddForm onSubmit={this.onSubmitHendler}>
-                <label htmlFor={this.nameInputId}>Name</label>
-                <input
-                    type="text"
-                    name="name"
-                    pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-                    title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-                    id={this.nameInputId}
-                    required
-                    onChange={this.chageHendler}
-                    value={this.state.name}
-                /> 
-                <br />
-                <label htmlFor={this.telephoneInputId}>Telephone</label>  
-                <input
-                    type="tel"
-                    name="number"
-                    pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-                    title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-                    required
-                    onChange={this.chageHendler}
-                    value={this.state.number}
-                    id={this.telephoneInputId}
-                />
-                <br />
-                <button type="submit" disabled={this.state.name === "" || this.state.number === ""}>Add contact</button>
-            </AddForm>
-        )
-    }
+
+    return(
+        <AddForm onSubmit={onSubmitHendler}>
+            <label htmlFor={nameInputId.current}>Name</label>
+            <input
+                type="text"
+                name="name"
+                pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                id={nameInputId.current}
+                required
+                onChange={chageHendler}
+                value={name}
+            /> 
+            <br />
+            <label htmlFor={telephoneInputId.current}>Telephone</label>  
+            <input
+                type="tel"
+                name="number"
+                pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+                required
+                onChange={chageHendler}
+                value={number}
+                id={telephoneInputId.current}
+            />
+            <br />
+            <button type="submit" disabled={name === "" || number === ""}>Add contact</button>
+        </AddForm>
+    )
 }
 
 AddNumberForm.propTypes = {
